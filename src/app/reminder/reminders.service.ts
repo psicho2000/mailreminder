795a792297effa1b89d6b869834ca624cabddff9
Markus Meier <markus.d.meier@gmail.com>
2019-07-02 16:09:58 +0200

    linting

--- src/app/reminder/reminders.service.ts
+++ src/app/reminder/reminders.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Reminder} from '../model/reminder';
-import {map} from "rxjs/operators";
+import {map} from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class RemindersService {
