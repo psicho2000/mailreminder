04d489de6223a6d51517d803c064582a175236dd
Markus Meier <markus.d.meier@gmail.com>
2019-07-02 15:27:39 +0200

    reformat

--- src/app/reminder/reminders.datasource.ts
+++ src/app/reminder/reminders.datasource.ts
@@ -4,28 +4,23 @@ import {Reminder} from '../model/reminder';
 import {RemindersService} from './reminders.service';
 import {catchError, finalize} from 'rxjs/operators';
 
-
 export class RemindersDataSource implements DataSource<Reminder> {
 
   private remindersSubject = new BehaviorSubject<Reminder[]>([]);
-
   private loadingSubject = new BehaviorSubject<boolean>(false);
-
   public loading$ = this.loadingSubject.asObservable();
 
   constructor(private remindersService: RemindersService) {
   }
 
   loadReminders(resultSize: number) {
-
     this.loadingSubject.next(true);
-
-    this.remindersService.findReminders(resultSize).pipe(
-      catchError(() => of([])),
-      finalize(() => this.loadingSubject.next(false))
-    )
+    this.remindersService.findReminders(resultSize)
+      .pipe(
+        catchError(() => of([])),
+        finalize(() => this.loadingSubject.next(false))
+      )
       .subscribe(reminders => this.remindersSubject.next(reminders));
-
   }
 
   connect(collectionViewer: CollectionViewer): Observable<Reminder[]> {
--- src/app/reminder/reminders.service.ts
+++ src/app/reminder/reminders.service.ts
@@ -2,20 +2,16 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Reminder} from '../model/reminder';
-import {map} from 'rxjs/operators';
+import {map} from "rxjs/operators";
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class RemindersService {
 
   constructor(private http: HttpClient) {
   }
 
   findReminders(resultSize = 10): Observable<Reminder[]> {
-
     const payload = 'payload';
-
     return this.http.get('/reminders/${resultSize}').pipe(
       map(res => res[payload])
     );
