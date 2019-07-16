698471203790d8812525f8d540d882f65f925d97
Markus Meier <markus.d.meier@gmail.com>
2019-07-16 18:55:28 +0200

    write persons as chips

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {ReminderComponent} from './components/reminder/reminder.component';
 import {reminderService} from './services/reminder.service';
-import {MatInputModule, MatTableModule} from '@angular/material';
+import {MatChipsModule, MatInputModule, MatTableModule} from '@angular/material';
 import {CommonModule} from '@angular/common';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HttpClientModule} from '@angular/common/http';
@@ -21,6 +21,7 @@ import {HttpClientModule} from '@angular/common/http';
     MatInputModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    MatChipsModule
   ],
   providers: [reminderService],
   bootstrap: [AppComponent]
